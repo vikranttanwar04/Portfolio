7bad41ec850f71d72904fe3677362975582667e7
vikranttanwar04 <151028037+vikranttanwar04@users.noreply.github.com>
2025-04-03 15:43:17 +0530

    Update Image.jsx

--- src/components/ImageComp/Image.jsx
+++ src/components/ImageComp/Image.jsx
@@ -2,6 +2,6 @@ import './Image.css'
 
 export default function Image(){
     return(
-        <img src="src\assets\vikrantTanwar.jpg" alt="Vikrant Tanwar"/>
+        <img src="\src\assets\vikrantTanwar.jpg" alt="Vikrant Tanwar"/>
     )
 }
