a354dd248f5f4bb75d212a0be0ffd9478ce0b46d
vikranttanwar04 <151028037+vikranttanwar04@users.noreply.github.com>
2025-04-11 10:57:10 +0530

    Update Thanks.jsx

--- src/components/Contact/Thanks.jsx
+++ src/components/Contact/Thanks.jsx
@@ -1,1 +1,9 @@
+import Footer from "../Footer/Footer";
 
+export default function Thanks(){
+    return(
+        <>
+            <Footer title="Thankyou" about="Message has been sent succussfully." btnText="Back to contact page" link="contact" />
+        </>
+    )
+}
