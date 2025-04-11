765a94e2904928aecf26295752b8358489dbd918
vikranttanwar04 <151028037+vikranttanwar04@users.noreply.github.com>
2025-04-11 10:58:15 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import Qualifications from './components/Qualifications/Qualifications.jsx';
 import Contact from './components/Contact/Contact.jsx';
 import NotFound from './components/PageNotFound/NotFound.jsx';
 import Work from './components/Work/Work.jsx';
+import Thanks from './components/Contact/Thankyou.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -21,6 +22,7 @@ createRoot(document.getElementById('root')).render(
         <Route path='/qualifications' element={<Qualifications />} />
         <Route path='/contact' element={<Contact />} />
         <Route path='/work' element={<Work />} />
+        <Route path='/thanks' element={<Thanks />} />
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
