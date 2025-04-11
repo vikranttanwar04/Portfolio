c1dd6ac9b4254cbfb563ed92965dcbb517b634b3
vikranttanwar04 <151028037+vikranttanwar04@users.noreply.github.com>
2025-04-11 10:54:12 +0530

    Update Contact.jsx

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,43 +1,44 @@
 import { useState } from 'react';
 import Footer from '../Footer/Footer';
 import './contact.css';
-import vikrantResume from "/src/assets/Vikrant Tanwar Resume.pdf"
+import vikrantResume from "/src/assets/vikrant Tanwar Resume.pdf"
+
 
 export default function Contact(){
 
-    const [alert, setAlert] = useState(false);
-    const [formData, setFormData] = useState({
-        user: "",
-        email: "",
-        msg: ""
-    })
+    // const [alert, setAlert] = useState(false);
+    // const [formData, setFormData] = useState({
+    //     user: "",
+    //     email: "",
+    //     msg: ""
+    // })
 
-    let [store, setStore] = useState([]);
+    // let [store, setStore] = useState([]);
 
-    function inputController(event){
-        let fieldName = event.target.name;
-        let fieldValue = event.target.value;
-        setFormData((prevState) =>{
-            return {...prevState, [fieldName] : fieldValue};
-        })
-    }
+    // function inputController(event){
+    //     let fieldName = event.target.name;
+    //     let fieldValue = event.target.value;
+    //     setFormData((prevState) =>{
+    //         return {...prevState, [fieldName] : fieldValue};
+    //     })
+    // }
     
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        setStore((prevState) =>{
-            return [...prevState, formData]
-        })
-        setFormData({
-            user: "",
-            email: "",
-            msg: ""
-        })
-        setAlert(true);
-    }
+    // const handleSubmit = (event) => {
+    //     event.preventDefault();
+    //     setStore((prevState) =>{
+    //         return [...prevState, formData]
+    //     })
+    //     setFormData({
+    //         user: "",
+    //         email: "",
+    //         msg: ""
+    //     })
+    //     setAlert(true);
+    // }
 
-    setTimeout(() => {
-        setAlert(false);
-    }, "5000");
+    // setTimeout(() => {
+    //     setAlert(false);
+    // }, "5000");
 
     return(
         <div id='contact-body'>
@@ -46,7 +47,7 @@ export default function Contact(){
                 <a className='text-light' href="https://www.linkedin.com/in/vikranttanwarrajput">Connect on Linkedin</a>
             </header>
 
-            {(() =>{
+            {/* {(() =>{
                 if(alert) {
                     return(
                         <div className='alert-box mb-2'>
@@ -54,20 +55,21 @@ export default function Contact(){
                         </div>
                     )
                 }
-            })()}
+            })()} */}
 
             <div>
                 <h2 className='text-light text-center'>Get in touch.</h2>
-                <form method="post" action='https://formsubmit.co/vikranttanwar124108@gmail.com' onSubmit={handleSubmit}>
+                <form method="post" action='https://formsubmit.co/vikranttanwar124108@gmail.com' >
                     <div className='row input-boxes'>
-                        <input className="p-2 me-1 mb-2 col-2 offset-4" type="text" placeholder="Name" name="user" value={formData.user} onChange={inputController} required/>
-                        <input className="p-2 mb-2 col-2" type="email" placeholder="Email" name="email" value={formData.email} onChange={inputController} required/>
+                        <input className="p-2 me-1 mb-2 col-2 offset-4" type="text" placeholder="Name" name="user" required/>
+                        <input className="p-2 mb-2 col-2" type="email" placeholder="Email" name="email" required/>
                     </div>
 
                     <div className='msg-box row'>
-                        <textarea className="p-2 mb-2 col-4 offset-4" rows="5" cols="44" placeholder="Message" name="msg" value={formData.msg} onChange={inputController} required/><br />
+                        <textarea className="p-2 mb-2 col-4 offset-4" rows="5" cols="44" placeholder="Message" name="msg" required/><br />
                     </div>
                     <div className='row'>
+                        <input type="hidden" name="_next" value="http://localhost:5173/thanks" />
                         <button type="Submit" className='send-btn p-2 col-4 offset-4' name='_captcha' value="false" >Send</button>
                     </div>
                 </form>
